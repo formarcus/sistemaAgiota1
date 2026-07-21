b37871bd59b801891509c6d5b7371d81e6362566
marcus oliveira <marcus.oliveira.dev@gmail.com>
2026-07-21 17:47:22 -0300

    implementando o restante das consultas do crud

--- backend/src/routers/userRoutes.ts
+++ backend/src/routers/userRoutes.ts
@@ -1,5 +1,11 @@
 import express from "express";
-import { getUsers, addUser, getUserById } from "../controllers/userController.ts"
+import { 
+    getUsers, 
+    addUser, 
+    getUserById,
+    updateUser,
+    deleteUser
+ } from "../controllers/userController.ts"
 
 
 const router = express.Router();
@@ -9,5 +15,7 @@ const router = express.Router();
 router.get('/', getUsers);
 router.get('/:id', getUserById);
 router.post('/', addUser);
+router.post('/:id', updateUser);
+router.delete('/:id', deleteUser);
 
 export { router } 
